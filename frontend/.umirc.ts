0bd7f64beb65d7295a6c9e723a272b5e5ece01ec
LiuKun <liukunup@163.com>
2025-05-27 00:17:39 +0800

    新增openapi支持

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -9,6 +9,14 @@ export default defineConfig({
   layout: {
     title: '@umijs/max',
   },
+  plugins: ['@umijs/max-plugin-openapi'],
+  openAPI: [
+    {
+      requestLibPath: "import { request } from '@umijs/max'",
+      schemaPath: 'http://localhost:8000/swagger/doc.json',
+      mock: false,
+    },
+  ],
   routes: [
     {
       path: '/',
